4c87c12412e84e6fd87a57e6384cdd488a49741d
hasan <hasandemir.5802@gmail.com>
2021-10-31 21:55:43 +0300

    Ödev

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
-const fn = function () {
-  console.log('fonction working');
-};
+const express = require('express');
+const app = express();
+const port = 3000;
+app.get('/',(req,res)=>{
+    const photo={
+        id:1,
+        name:'Photo Name',
+        description:'Photo Description'
+    }
+    res.send(photo)
+})
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı..`);
+});
