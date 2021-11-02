167579c82960ae6de80cc0ebcb9d3dfaeec7204a
hasan <hasandemir.5802@gmail.com>
2021-11-02 23:29:48 +0300

    Çalışmalar Sürüyor

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 const express = require('express');
+const ejs = require('ejs');
 const path = require('path');
 const app = express();
+//TEMPLATE ENGİNE
+app.set('view engine', 'ejs');
 
 //MİDDLEWARES
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-const port = 3000;
+//ROUTES
+const port = 3001;
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render('index');
 });
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/add', (req, res) => {
+  res.render('add');
+});
+
+app.post('/photos', (req, res) => {
+  console.log(req.body);
+  res.redirect('/');
+});
+
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda başlatıldı..`);
 });
