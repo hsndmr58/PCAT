8bd9caac7db31f7e29336f6f2130f9d1df71fa18
hasan <hasandemir.5802@gmail.com>
2021-11-06 00:58:30 +0300

    Tamamlandı

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
+const methodOverride = require('method-override');
 const ejs = require('ejs');
 const path = require('path');
+const fs = require('fs');
 const app = express();
-const Photo=require("./models/Photo")
-
+const Photo = require('./models/Photo');
+const photoControllers = require('./controllers/photoControllers');
+const pageControllers=require('./controllers/pageControllers')
 
 //connect DB
 
-mongoose.connect('mongodb://localhost/pcat-test-db',{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
+mongoose.connect('mongodb://localhost/pcat-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+
 });
 
 //TEMPLATE ENGİNE
@@ -20,42 +25,27 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(fileUpload());
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
+  })
+);
+
+const port = 3001;
 
 //ROUTES
-const port = 3001;
-app.get('/', async (req, res) => {
-  const photos=await Photo.find({})
-  res.render('index',{
-    photos
-    
-  });
- 
-});
 
+app.get('/', photoControllers.getAllPhotos);
+app.get('/photos/:id', photoControllers.getPhoto);
+app.post('/photos', photoControllers.createdPhoto);
+app.put('/photos/:id', photoControllers.updatePhoto);
+app.delete('/photos/:id', photoControllers.deletePhoto);
 
-app.get('/photos/:id', async (req, res) => {
-  //console.log(req.params.id)
-  //res.render('about');
-  const photo=await Photo.findById(req.params.id)
-  res.render('photo',{
-    photo
-  })
-});
+app.get('/about',pageControllers.getAboutPage);
+app.get('/add', pageControllers.getAddPage);
 
-
-
-
-app.get('/about', (req, res) => {
-  res.render('about');
-});
-app.get('/add', (req, res) => {
-  res.render('add');
-});
-
-app.post('/photos', async (req, res) => {
-  await Photo.create(req.body)
-  res.redirect('/');
-});
+app.get('/photos/edit/:id',pageControllers.getEditPage);
 
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda başlatıldı..`);
